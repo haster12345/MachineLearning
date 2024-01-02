03c647de0b33ffb1bf5567cfa99fa69e4f4f0dfb
haster12345 <tarunmankani25@gmail.com>
2024-01-02 23:03:51 +0000

    added TestData class which usees the test data to evaluate outputs using Mean Square Error

--- main.py
+++ main.py
@@ -123,8 +123,33 @@ class LinearRegression:
     def normal_equations(self):
         pass
 
-    def mean_square_error(self):
-        pass
+class TestData:
 
-x = LinearRegression(0.01, features_train=features_train, target_variables_train=target_variables_train).batch_gradient_descent()
+    def __init__(self, target_variables_test, features_test) -> None:
+        self.target_variables_test = np.array(target_variables_test)
+        self.features_test_no_intercept = np.array(features_test)
+        self.features_shape = np.shape(self.features_test_no_intercept)
+        self.features_test = np.c_[np.ones(self.features_shape[0]), self.features_test_no_intercept]
+
+        # print(self.features_test)
+
+    def mean_square_error(self):
+
+        vector_of_params_output = LinearRegression(0.00001, features_train=features_train, target_variables_train=target_variables_train).batch_gradient_descent()
+        hypothesis_value = 0
+        sum_se = 0
+
+        for i in range(self.features_shape[0]):
+            for j in range(len(vector_of_params_output)):
+                hypothesis_value += self.features_test[i][j] + vector_of_params_output[j]
+            
+            sum_se += (hypothesis_value - self.target_variables_test[i])**2
+        
+        return sum_se/self.features_shape[0]
+
+
+x = LinearRegression(0.001, features_train=features_train, target_variables_train=target_variables_train).batch_gradient_descent()
 print(x)
+
+y = TestData(target_variables_test=target_variables_test, features_test=features_test).mean_square_error()
+print(y)
