6c476b53fea1417cb45c111a401259dd900e672d
haster12345 <tarunmankani25@gmail.com>
2024-01-04 23:57:32 +0000

    dummy data for classification prbls

--- dummy_data.py
+++ dummy_data.py
@@ -22,3 +22,28 @@ def dummy_data_fn():
     df = pd.DataFrame(data = data_dict) 
 
     return df
+
+def polarised_fn(x, y):
+    if x*y >= 0:
+        return 1
+    else:
+        return 0
+
+
+def dummy_data_classication_fn():
+
+    x_values = [random.uniform(-1, 1) for _ in range(1000)]
+    y_values = [random.uniform(-1,-1) for _ in range(1000)]
+    
+    xy_values = []
+
+    for i in range(1000):
+        xy_values.append((x_values[i], y_values[i]))
+
+    pol_values = [polarised_fn(x,y) for (x,y) in xy_values]
+
+    data_dict = {'x_values' : x_values, 'y_values': y_values, 'polxy_values': pol_values}
+
+    df = pd.DataFrame(data = data_dict) 
+
+    return df 
