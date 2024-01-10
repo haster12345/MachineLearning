ad2f9212e397baf19335b809000a1740375e56a1
tarun <tarunmankani25@gmail.com>
2024-01-10 23:15:15 +0000

    cleanup

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-class LogiscticRegression:
 
+class LogisticRegression:
 
     def __init__(self, alpha, features_train, target_variables_train):
         """
@@ -24,9 +24,9 @@ class LogiscticRegression:
         self.number_of_inputs: int = len(self.features_train)
         self.number_of_parameters = len(self.features_train[0])
         self.initial_vector_of_parameters: np.array = np.zeros(self.number_of_parameters)
-        self.hypothesis_vector = np.ones(self.number_of_parameters) 
+        self.hypothesis_vector = np.ones(self.number_of_parameters)
         self.hypothesis_vector[0] = 0
-    
+
     def sigmoid_function(self, vector_of_parameters, row):
 
         dot_product = 0
@@ -34,31 +34,32 @@ class LogiscticRegression:
         for i in range(self.number_of_parameters):
             dot_product += vector_of_parameters[i] * self.features_train[row][i]
 
-        return 1/(1 + np.exp(dot_product))
-    
+        return 1 / (1 + np.exp(dot_product))
+
     def threshold_function(self, vector_of_parameters, row):
 
         dot_product = 0
-        
+
         for i in range(self.number_of_parameters):
             dot_product += vector_of_parameters[i] * self.features_train[row][i]
-        
+
         if dot_product >= 0:
             return 1
-        
+
         else:
             return 0
 
-
-    def stochastic_gradient_descent(self, abs_diff = 0.0001):
+    def stochastic_gradient_descent(self, abs_diff=0.0001):
 
         vector_of_parameters = self.initial_vector_of_parameters
         for i in range(self.number_of_outputs):
             for j in range(self.number_of_parameters):
 
                 theta_j = vector_of_parameters[j]
-                theta_j = theta_j +  self.alpha * (self.target_variables_train[i] - self.threshold_function(vector_of_parameters, i )) * self.features_train[i][j]
-    
+                theta_j = theta_j + self.alpha * (
+                            self.target_variables_train[i] - self.threshold_function(vector_of_parameters, i)) * \
+                          self.features_train[i][j]
+
                 if abs(vector_of_parameters[j] - theta_j) < abs_diff:
                     vector_of_parameters[j] = theta_j
 
@@ -68,7 +69,7 @@ class LogiscticRegression:
 class SoftmaxRegression:
 
     def __init__(self, alpha, features_train, target_variables_train) -> None:
-        
+
         self.alpha: float = alpha
         self.features_train_no_intercept: np.array[np.array] = np.array(features_train)
         self.features_shape = np.shape(self.features_train_no_intercept)
@@ -77,29 +78,29 @@ class SoftmaxRegression:
         self.number_of_outputs: int = len(self.target_variables_train)
         self.number_of_inputs: int = len(self.features_train)
         self.number_of_parameters = len(self.features_train[0])
-        self.number_of_examples = len(self.features_shape[1])
-        self.initial_matrix_of_parameters: np.array[np.array] = np.zeros((self.number_of_parameters, self.number_of_outputs))
+        self.number_of_examples = self.features_shape[1]
+        self.initial_matrix_of_parameters: np.array[np.array] = np.zeros(
+            (self.number_of_parameters, self.number_of_outputs))
         self.hypothesis_vector = np.ones(self.number_of_parameters)
 
+    def stochastic_gradient_descent(self, abs_diff=0.00001):
 
-    def stochastic_gradient_descent(self,abs_diff=0.00001):
-
-        matrix_of_parameters = np.zeros((self.number_of_parameters,self.number_of_outputs))
+        matrix_of_parameters = np.zeros((self.number_of_parameters, self.number_of_outputs))
         for l in range(self.number_of_outputs):
             vector_of_parameters = self.initial_matrix_of_parameters[l]
             for i in range(self.number_of_examples):
-                    for j in range(self.number_of_parameters):
-                        theta_j = vector_of_parameters[j]
-                        theta_j = theta_j + self.alpha * self.deriv_cost_function(vector_of_parameters[j],l)
+                for j in range(self.number_of_parameters):
+                    theta_j = vector_of_parameters[j]
+                    theta_j = theta_j + self.alpha * self.deriv_cost_function(vector_of_parameters[j], l)
 
-                        if abs(vector_of_parameters[j] - theta_j) < abs_diff:
-                            vector_of_parameters[j] = theta_j
-                            continue
+                    if abs(vector_of_parameters[j] - theta_j) < abs_diff:
+                        vector_of_parameters[j] = theta_j
+                        continue
 
-            matrix_of_parameters[l] =  matrix_of_parameters 
+            matrix_of_parameters[l] = matrix_of_parameters
 
-        return matrix_of_parameters 
-    
+        return matrix_of_parameters
+
     @staticmethod
     def indicator_function(x1, x2):
         if x1 == x2:
@@ -107,9 +108,7 @@ class SoftmaxRegression:
         else:
             return 0
 
-
     def deriv_cost_function(self, vector_of_parameters, k):
-        
         sum = 0
         for i in range(self.number_of_examples):
             sum1 = 1
@@ -117,9 +116,8 @@ class SoftmaxRegression:
             for j in range(self.number_of_outputs):
                 sum1 += np.exp(np.dot(vector_of_parameters, self.features_train[i]))
 
-            sum += self.features_train[i] * (self.indicator_function(self.target_variables_train[i],k) 
-                                             - np.exp(np.dot(vector_of_parameters,self.features_train[i]))/(
-                                                sum1 
+            sum += self.features_train[i] * (self.indicator_function(self.target_variables_train[i], k)
+                                             - np.exp(np.dot(vector_of_parameters, self.features_train[i])) / (
+                                                 sum1
                                              ))
-
         return -sum
--- testmodel.py
+++ testmodel.py
@@ -1,6 +1,6 @@
 import numpy as np
 from linear_regression import LinearRegression
-from classification import LogiscticRegression
+from classification import LogisticRegression
 
 
 class TestModel:
